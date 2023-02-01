75d8b9d73029ddf7189d8076fc72b67285a3c984
shilinhui <shilinhui@eben.cn>
2023-02-01 15:55:15 +0800

    add setWindowStatusBarColor

--- utilslib/src/main/java/com/self/myapplication/DisplayUtils.java
+++ utilslib/src/main/java/com/self/myapplication/DisplayUtils.java
@@ -4,9 +4,11 @@ import static android.content.Context.WINDOW_SERVICE;
 
 import android.app.Activity;
 //import android.app.WindowConfiguration;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Point;
+import android.os.Build;
 import android.view.Display;
 import android.view.View;
 import android.view.Window;
@@ -130,4 +132,34 @@ public class DisplayUtils {
         }
         return false;
     }**/
+
+    /**
+     * 设置Activity对应的顶部状态栏的颜色
+     */
+    public static void setWindowStatusBarColor(Activity activity, int colorResId) {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                Window window = activity.getWindow();
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.setStatusBarColor(activity.getResources().getColor(colorResId));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 设置Dialog对应的顶部状态栏的颜色
+     */
+    public static void setWindowStatusBarColor(Dialog dialog, int colorResId) {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                Window window = dialog.getWindow();
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.setStatusBarColor(dialog.getContext().getResources().getColor(colorResId));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
